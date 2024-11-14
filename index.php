0323dc11eb9d374c094eca701ad1459d3dca9ea1
roy157 <78753915+roy157@users.noreply.github.com>
2024-11-14 18:11:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (isset($_POST['cerrar_sesion'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Principal</title>
+    <title>HOTEL RESERVAS</title>
 </head>
 <body>
     <h1>Bienvenido al Sistema de Hotel</h1>
